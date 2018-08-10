ec3509b8d303334bc8f720dd55b17681a68ff1ad
willix-win <benavidesb@unicauca.edu.co>
2018-08-10 14:02:44 -0500

    ep 11 Validating signup form

--- includes/signup-form.php
+++ includes/signup-form.php
@@ -1,5 +1,33 @@
 <?php
+	if(isset($_POST['signup'])) {
+		$screenName = $_POST['screenName'];
+		$password = $_POST['password'];
+		$email = $_POST['email'];
+		$error = ''; 
 
+		if(empty($screenName) or empty($password) or empty($email)) {
+			$error = 'All fields are required';
+		} else {
+			$email = $getFromU->checkInput($email);
+			$screenName = $getFromU->checkInput($screenName);
+			$password = $getFromU->checkInput($password);
+
+			if(!filter_var($email)) {
+				$error = 'Invalid email format';
+			} else if(strlen($screenName) > 20) {
+				$error = 'Name must be betweet in 6-20 characters';
+			} else if(strlen($password) < 5) {
+				$error = 'Password is too short';
+			} else {
+				if ($getFromU->checkEmail($email) === true) {
+					$error = 'Email is already in use';
+				} else {
+
+				}
+			}
+		}
+
+	}
 ?>
 <form method="post">
 <div class="signup-div"> 
@@ -17,11 +45,13 @@
 		<li>
 			<input type="submit" name="signup" Value="Signup for Twitter">
 		</li>
+		<?php 
+			if(isset($error)) {
+				echo '<li class="error-li">
+	  					<div class="span-fp-error">'.$error.'</div>
+	 					</li> ';
+			}
+		?>
 	</ul>
-	<!--
-	 <li class="error-li">
-	  <div class="span-fp-error"></div>
-	 </li> 
-	-->
 </div>
 </form>
